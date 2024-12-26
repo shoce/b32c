d4550d9788db897287e2928ac761fb3e97db1829
shoce <yogze@icloud.com>
2024-12-26 12:42:54 +0530

    b32c

---
+++ b32c.go
@@ -0,0 +1,33 @@
+/*
+history:
+2020/3/31 v1
+
+GoGet
+GoFmt
+GoBuildNull
+GoBuild
+*/
+
+package main
+
+import (
+	"encoding/base32"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	crockford = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+)
+
+func main() {
+	bb, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	e := base32.NewEncoding(crockford)
+	s := e.EncodeToString(bb)
+	fmt.Println(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shoce/b32c
+
+go 1.23.4
